b5b6545c524e03248cf2809e8d5ad8796456e460
Shmath64 <64mdavis@gmail.com>
2023-09-29 14:38:59 -0400

    Zombies now have timer and deal damage to player

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -3,22 +3,26 @@ var DeathIndex = 2 #deprecated (Evan's code)
 var max_health = 100
 var speed = 200
 var stunned = false
+var player : Object
 
-func _physics_process(_delta):
-	var Player = get_parent().get_node("Player")
+func _ready():
+	player = get_parent().get_node("Player")
+
+func _process(_delta):
+	
 	
 	#position += (Player.position - position) / 50
 	#look_at(Player.position)
-	
-	set_velocity(Vector2(speed, 0).rotated(get_angle_to(Player.position)))
-	
-	move_and_slide()
-	var collision = get_last_slide_collision()
-	if collision:
-		if collision.get_collider().has_method("touch_zombie"):
-			collision.get_collider().call("touch_zombie") 
-			stunned = true
-			print("Stunned")
+	if !stunned:
+		set_velocity(Vector2(speed, 0).rotated(get_angle_to(player.position)))
+		move_and_slide()
+		var collision = get_last_slide_collision()
+		if collision:
+			if collision.get_collider().has_method("touch_zombie"):
+				collision.get_collider().call("touch_zombie") 
+				stunned = true
+				print("Stunned")
+				$StunTimer.start()
 	
 	
 func hit(damage : float):
@@ -44,3 +48,7 @@ func die():
 
 func _on_flash_color_timer_timeout():
 	$Sprite2D.modulate = Color(1,1,1)
+
+
+func _on_stun_timer_timeout():
+	stunned = false
